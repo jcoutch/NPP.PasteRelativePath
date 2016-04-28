a2709f2ff037f8aeeb73b56526cdcaa9cc7a5ed8
Joe Coutcher <joe.coutcher@maritzcx.com>
2016-04-28 12:39:16 -0400

    Added support for case sensitivity/insensitivity

--- NPP.PasteRelativePath/Main.cs
+++ NPP.PasteRelativePath/Main.cs
@@ -14,7 +14,7 @@ namespace NPP.PasteRelativePath
         #region " Fields "
         internal const string PluginName = "NPP.PasteRelativePath";
         static string iniFilePath = null;
-        //static bool someSetting = false;
+        static bool isCaseSensitive = false;
         static frmMyDlg frmMyDlg = null;
         static int idMyDlg = -1;
         static Bitmap tbBmp = Properties.Resources.star;
@@ -24,14 +24,15 @@ namespace NPP.PasteRelativePath
         #region " StartUp/CleanUp "
         internal static void CommandMenuInit()
         {
-            //StringBuilder sbIniFilePath = new StringBuilder(Win32.MAX_PATH);
-            //Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_GETPLUGINSCONFIGDIR, Win32.MAX_PATH, sbIniFilePath);
-            //iniFilePath = sbIniFilePath.ToString();
-            //if (!Directory.Exists(iniFilePath)) Directory.CreateDirectory(iniFilePath);
-            //iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
-            //someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
+            StringBuilder sbIniFilePath = new StringBuilder(Win32.MAX_PATH);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_GETPLUGINSCONFIGDIR, Win32.MAX_PATH, sbIniFilePath);
+            iniFilePath = sbIniFilePath.ToString();
+            if (!Directory.Exists(iniFilePath)) Directory.CreateDirectory(iniFilePath);
+            iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
+            isCaseSensitive = (Win32.GetPrivateProfileInt("FileSystem", "CaseSensitive", 0, iniFilePath) != 0);
 
             PluginBase.SetCommand(0, "Paste Relative", myMenuFunction, new ShortcutKey(true, false, true, Keys.V));
+            PluginBase.SetCommand(1, "Use case sensitive paths", changeCaseSensitivity, isCaseSensitive);
         }
         internal static void SetToolBarIcon()
         {
@@ -44,12 +45,12 @@ namespace NPP.PasteRelativePath
         }
         internal static void PluginCleanUp()
         {
-            //Win32.WritePrivateProfileString("SomeSection", "SomeKey", someSetting ? "1" : "0", iniFilePath);
+            Win32.WritePrivateProfileString("FileSystem", "CaseSensitive", isCaseSensitive ? "1" : "0", iniFilePath);
         }
         #endregion
 
         #region " Menu functions "
-        internal static string RelativePath(string absPath, string relTo)
+        internal static string RelativePath(string absPath, string relTo, bool isCaseSensitive)
         {
             var absDirs = absPath.Split('\\');
             var relDirs = relTo.Split('\\');
@@ -60,7 +61,7 @@ namespace NPP.PasteRelativePath
             // Find common root 
             for (index = 0; index < len; index++)
             {
-                if (absDirs[index] == relDirs[index])
+                if (string.Equals(absDirs[index], relDirs[index], isCaseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase))
                     lastCommonRoot = index;
                 else break;
             }
@@ -85,6 +86,14 @@ namespace NPP.PasteRelativePath
             return relativePath.ToString();
         }
 
+        internal static void changeCaseSensitivity()
+        {
+            isCaseSensitive = !isCaseSensitive;
+
+            var myMenu = Win32.GetMenu(PluginBase.nppData._nppHandle);
+            Win32.CheckMenuItem(myMenu, PluginBase._funcItems.Items[1]._cmdID, isCaseSensitive ? Win32.MF_CHECKED : Win32.MF_UNCHECKED );
+        }
+
         internal static void myMenuFunction()
         {
             var clipboardText = Clipboard.GetText();
@@ -94,7 +103,7 @@ namespace NPP.PasteRelativePath
             {
                 Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_GETCURRENTDIRECTORY, Win32.MAX_PATH, filePath);
 
-                var relativePath = RelativePath(filePath.ToString(), Clipboard.GetText());
+                var relativePath = RelativePath(filePath.ToString(), Clipboard.GetText(), isCaseSensitive);
 
                 Win32.SendMessage(PluginBase.nppData._scintillaMainHandle, SciMsg.SCI_ADDTEXT, relativePath.Length, relativePath);
             }
--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Notepad++ Plugin for pasting relative paths (written in C#)
 
 To install, download `NPP.PasteRelativePath.dll` from the release folder and drop it in `C:\Program Files (x86)\Notepad++\plugins`.  If you have Notepad++ started, you'll have to restart it.
 
-To use, copy a path into your clipboard, open a document, and press `Ctrl+Shift+V` (or Plugins -> NPP.PasteRelativePath -> Paste Relative), and the path pasted will be relative to the currently opened document.
+To use, copy a path into your clipboard, open a document, and press `Ctrl+Shift+V` (or `Plugins -> NPP.PasteRelativePath -> Paste Relative`), and the path pasted will be relative to the currently opened document.
+
+By default, path matching is done with case insensitivity (for Windows users.)  If you need to support case sensitive paths, you can enable it by selecting `Plugins -> NPP.PasteRelativePath -> Use case sensitive paths`.
